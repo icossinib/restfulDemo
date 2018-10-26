6b0d99d642b69f4064c2588a0039171269e19508
Ignacio Cossini Benchimol <jefazo666@gmail.com>
2018-10-26 20:39:45 +0200

    Initial commit

---
+++ src/main/java/com/cossinisoftware/restfullDemo/RestfullDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cossinisoftware.restfullDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullDemoApplication.class, args);
+	}
+}
