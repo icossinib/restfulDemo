201892ee04c58d90d85e68ebfe42ca77a6a27562
Ignacio Cossini Benchimol <jefazo666@gmail.com>
2018-11-03 13:24:59 +0100

    Updates after main object changed

--- src/main/java/com/cossinisoftware/restfullDemo/Employee.java
+++ src/main/java/com/cossinisoftware/restfullDemo/Employee.java
@@ -11,11 +11,28 @@ import javax.persistence.Id;
 public class Employee {
 
 	private @Id @GeneratedValue Long id;
-	private String name;
+	private String firstName;
+	private String surname;
 	private String role;
 
-	public Employee(String name, String role) {
-		this.name = name;
+	public Employee(String firstName, String surname, String role) {
+		this.firstName = firstName;
+		this.surname = surname;
 		this.role = role;
+	}
+
+	public Employee(String name, String role){
+		this.setName(name);
+		this.role = role;
+	}
+
+	public void setName(String name) {
+		String splittedName[] = name.split(" ");
+		this.firstName = splittedName[0];
+		this.surname = splittedName[1];
+	}
+
+	public String getName() {
+		return firstName + " " + surname;
 	}
 }
--- src/main/java/com/cossinisoftware/restfullDemo/EmployeeController.java
+++ src/main/java/com/cossinisoftware/restfullDemo/EmployeeController.java
@@ -2,13 +2,13 @@ package com.cossinisoftware.restfullDemo;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -43,22 +43,27 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) throws URISyntaxException {
+        Resource<Employee> resource = assembler.toResource(repository.save(newEmployee));
+        return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
+
     @PutMapping("/employees/{id}")
-    Employee updateEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-        return repository.findById(id)
-                .map(employee -> {
-                    employee.setName(newEmployee.getName());
+    Resource<Employee> updateEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        Resource<Employee> resource = assembler.toResource(
+                repository.findById(id).map( employee -> {
+                    employee.setFirstName(newEmployee.getFirstName());
+                    employee.setSurname(newEmployee.getSurname());
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
                 })
-                .orElseGet(() ->{
+                .orElseGet(() -> {
                     newEmployee.setId(id);
                     return repository.save(newEmployee);
-                });
+                }));
+
+        return resource;
     }
 
     @DeleteMapping("/employees/{id}")
